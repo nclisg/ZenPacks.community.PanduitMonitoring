6ecd8287ef2a3c9880743dae289ea44bb0bfa411
Zenoss Account <zenoss@zenoss-dev03.ncl.ac.uk>
2015-10-26 10:05:02 +0000

    remove manufacturer

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.PanduitMonitoring"
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 AUTHOR = "Nick Muir"
 LICENSE = "LGPL"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
