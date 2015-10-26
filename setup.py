37c2815fdc2832f1dc015e32c22ad9a825d47d53
Zenoss Account <zenoss@zenoss-dev03.ncl.ac.uk>
2015-10-26 15:50:08 +0000

    increased version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.PanduitMonitoring"
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 AUTHOR = "Nick Muir"
 LICENSE = "LGPL"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
